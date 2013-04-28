1e9fa94c967be78568bc91307902869e3aa5985a
Ben Peachey <potherca@gmail.com>
2013-04-28 19:18:34 +0200

    Minor whitespace fix.

--- 100-Best-Novels-of-the-20th-Century.php
+++ 100-Best-Novels-of-the-20th-Century.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8" />
-	<title>100 Best Novels of the 20th Century</title>
-	<link rel="stylesheet" type="text/css" href="base.css"/>
+    <meta charset="utf-8" />
+    <title>100 Best Novels of the 20th Century</title>
+    <link rel="stylesheet" type="text/css" href="base.css"/>
 </head>
 
 <body>
-	<h1>100 Best Novels of the 20th Century</h1>
+    <h1>100 Best Novels of the 20th Century</h1>
 
-	<p>
-		The following list combines both the Readers and the Board list of the
-		<a href="http://www.modernlibrary.com/top-100/100-best-novels/">Modern
-		Library top-100-best-novels lists</a>, re-indexed by combining the
-		position of books on both lists.  Because of this, several novels may
-		occupy the same space. When this occurs, the novels are not displayed
-		in any particular order.
-	</p>
-	<p>
-		All novels have been color coded to state in which list they originally appeared:
-	</p>
-	<ul>
-		<li class="boardreaders"><span class="novel">Appears in both the Board and the Readers List</span></li>
-		<li class="board"><span class="novel">Appears only in the Board List</span></li>
-		<li class="readers"><span class="novel">Appears only in the Readers List</span></li>
-	</ul>
+    <p>
+        The following list combines both the Readers and the Board list of the
+        <a href="http://www.modernlibrary.com/top-100/100-best-novels/">Modern
+        Library top-100-best-novels lists</a>, re-indexed by combining the
+        position of books on both lists.  Because of this, several novels may
+        occupy the same space. When this occurs, the novels are not displayed
+        in any particular order.
+    </p>
+    <p>
+        All novels have been color coded to state in which list they originally appeared:
+    </p>
+    <ul>
+        <li class="boardreaders"><span class="novel">Appears in both the Board and the Readers List</span></li>
+        <li class="board"><span class="novel">Appears only in the Board List</span></li>
+        <li class="readers"><span class="novel">Appears only in the Readers List</span></li>
+    </ul>
 
     <p>
         Books that are available for free from <a href="//www.gutenberg.org"
@@ -36,10 +36,10 @@
     </ol>
 
     <div class="legend">
-	<h2>Novels</h2>
-	<ul>
-		<li id="both">appearing in both lists</li>
-		<li id="total">Total</li>
+    <h2>Novels</h2>
+    <ul>
+        <li id="both">appearing in both lists</li>
+        <li id="total">Total</li>
         <li id="gutenberg_total">
             <hr/>
             available from <a href="//www.gutenberg.org">Project Gutenberg</a>
@@ -47,13 +47,13 @@
     </ul>
 
     <?if(NovelList::readAnyBooks() === true):?>
-	<h3>Novels I have Read</h3>
-	<ul>
-		<li class="boardreaders" id="read_both"><span class="novel">appearing on both list</span> </li>
-		<li class="board" id="read_board"><span class="novel">appearing on the board list</span> </li>
-		<li class="readers" id="read_readers"><span class="novel">appearing on the readers list</span> </li>
-		<li id="read_total">Total</li>
-	</ul>
+    <h3>Novels I have Read</h3>
+    <ul>
+        <li class="boardreaders" id="read_both"><span class="novel">appearing on both list</span> </li>
+        <li class="board" id="read_board"><span class="novel">appearing on the board list</span> </li>
+        <li class="readers" id="read_readers"><span class="novel">appearing on the readers list</span> </li>
+        <li id="read_total">Total</li>
+    </ul>
     <?endif?>
 </div>
 
@@ -185,7 +185,7 @@ class NovelList
 
         foreach($this->getCombinedList() as $t_sBook => $t_iScore) {
             if($iPreviousScore && $t_iScore !== $iPreviousScore) {
-                $sContent .='</li><li>';
+                $sContent .='</li>' . "\n" . '        <li>';
             }
 
             list($sNovel, $sAuthor) = explode(' by ', $t_sBook);
@@ -202,8 +202,8 @@ class NovelList
                       . '">'
                     :''
                 )
-                . '<span class="novel">' . $sNovel . '</span> by
-                   <span class="author">' . $sAuthor . '</span>'
+                . '<span class="novel">' . $sNovel . '</span> by '
+                . '<span class="author">' . $sAuthor . '</span>'
                 . ($bAvailableFromGutenburg
                     ?'</a>'
                     :''
@@ -214,7 +214,7 @@ class NovelList
             $iPreviousScore = $t_iScore;
         }
 
-        $sContent .= '</li>';
+        $sContent .= '</li>' . "\n";
 
         return $sContent;
     }
